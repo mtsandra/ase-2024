20c9ffe27c9365faecec099912bd59d19439f683
mtsandra <tengofma@gmail.com>
2024-02-08 21:59:23 -0500

    exercise2

--- src/ring_buffer.rs
+++ src/ring_buffer.rs
@@ -1,64 +1,77 @@
 pub struct RingBuffer<T> {
     // TODO: fill this in.
+    buffer: Vec<T>,
+    head: usize,
+    tail: usize
 }
 
 impl<T: Copy + Default> RingBuffer<T> {
     pub fn new(length: usize) -> Self {
         // Create a new RingBuffer with `length` slots and "default" values.
         // Hint: look into `vec!` and the `Default` trait.
-        todo!()
+        RingBuffer { buffer: vec![T; capacity],
+             head: 0, 
+             tail: 0 }
     }
 
     pub fn reset(&mut self) {
         // Clear internal buffer and reset indices.
-        todo!()
+        self.buffer.fill(T);
+        self.head = 0 ;
+        self.tail=0;
     }
 
     // `put` and `peek` write/read without advancing the indices.
     pub fn put(&mut self, value: T) {
-        todo!()
+        self.buffer[self.head] = value
     }
 
     pub fn peek(&self) -> T {
-        todo!()
+        self.buffer[self.tail]
     }
 
     pub fn get(&self, offset: usize) -> T {
-        todo!()
+        self.buffer[(self.tail + offset) % self.capacity()]
     }
 
     // `push` and `pop` write/read and advance the indices.
     pub fn push(&mut self, value: T) {
-        todo!()
+        self.buffer[self.head] = value;
+        self.head = (self.head + 1) % self.capacity();
     }
 
     pub fn pop(&mut self) -> T {
-        todo!()
+        let value = self.buffer[self.tail];
+        self.tail = (self.tail + 1) % self.capacity()
     }
 
     pub fn get_read_index(&self) -> usize {
-        todo!()
+        self.tail
     }
 
     pub fn set_read_index(&mut self, index: usize) {
-        todo!()
+        self.tail = index % self.capacity()
     }
 
     pub fn get_write_index(&self) -> usize {
-        todo!()
+        self.head
     }
 
     pub fn set_write_index(&mut self, index: usize) {
-        todo!()
+        self.head = index % self.capacity()
     }
 
     pub fn len(&self) -> usize {
         // Return number of values currently in the buffer.
-        todo!()
+        if self.head >= self.tail{
+            self.head - self.tail
+        } else {
+            self.head + self.capacity() - self.tail
+        }
     }
 
     pub fn capacity(&self) -> usize {
         // Return the length of the internal buffer.
-        todo!()
+        self.buffer.len()
     }
 }
